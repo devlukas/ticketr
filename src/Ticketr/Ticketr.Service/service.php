dc65e5c058d09463e121e5464ab6ff97079c35e7
BrunnerLivio <livio.brunner@hotmail.com>
2016-05-05 13:21:50 +0200

    Changed functions.php to a class "Controller"

--- src/Ticketr/Ticketr.Service/service.php
+++ src/Ticketr/Ticketr.Service/service.php
@@ -1,22 +1,14 @@
 <?php 
 	require_once("includes/dbconnect.php");
-	require_once("includes/functions.php");
-	
-	$allowedFunctions = array(
-		"getAllKunden",
-		"getAllMitarbeiter",
-		"getkundendetail",
-		"getmitarbeiterdetail",
-		"addMitarbeiter",
-		"addKunde"
-	);
-	
+	require_once("includes/controller.php");
+	$controller = new Controller();
+	$allowedFunctions = get_class_methods ($controller);
 	
 	//Check if this method is allowed
 	if(isset($_GET["method"]) && in_array($_GET["method"], $allowedFunctions))
 	{
 		//Call service method
-		call_user_func($_GET["method"]);
+		call_user_func(array($controller, $_GET["method"]));
 	}
 	else{
 		header("HTTP/1.0 404 Not Found");
