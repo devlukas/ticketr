7a90557321fee048d28c665a7b01349b7dda762e
BrunnerLivio <livio.brunner@hotmail.com>
2016-05-22 17:26:40 +0200

    Fixed login bug

--- src/Ticketr/Ticketr.Businesslogik/TicketSystem.cs
+++ src/Ticketr/Ticketr.Businesslogik/TicketSystem.cs
@@ -71,11 +71,12 @@ namespace Ticketr.Businesslogik
         /// <returns></returns>
         public bool Login(string eMail, string password)
         {
+            bool authenticated = service.Login(eMail, password);
             currentUser = new Mitarbeiter(service.GetCurrentMitarbeiterDetail());
-            return service.Login(eMail, password);
+            return authenticated;
         }
 
 
-        
+
     }
 }
