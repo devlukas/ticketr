9be8e95fb40c49f4a043a28633c762b0fa99766c
BrunnerLivio <livio.brunner@hotmail.com>
2016-05-22 16:44:42 +0200

    Buisnesslogik tickets werden nun abgefüllt von den DTOS

--- src/Ticketr/Ticketr.Businesslogik/Kategorie.cs
+++ src/Ticketr/Ticketr.Businesslogik/Kategorie.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Ticketr.Schnittstellen.Dto;
 
 namespace Ticketr.Businesslogik
 {
     public class Kategorie
     {
+        private Schnittstellen.Dto.Kategorie kategorie;
+
+        public Kategorie(Schnittstellen.Dto.Kategorie kategorie)
+        {
+            this.kategorie = kategorie;
+        }
     }
 }
