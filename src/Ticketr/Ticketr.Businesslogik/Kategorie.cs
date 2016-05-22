f8a11967591412cd5b7e8df6aa2ab3d9c05aa04c
BrunnerLivio <livio.brunner@hotmail.com>
2016-05-22 17:27:21 +0200

    Kopieren der DTOS bei Kategorie

--- src/Ticketr/Ticketr.Businesslogik/Kategorie.cs
+++ src/Ticketr/Ticketr.Businesslogik/Kategorie.cs
@@ -7,12 +7,25 @@ using Ticketr.Schnittstellen.Dto;
 
 namespace Ticketr.Businesslogik
 {
+    /// <summary>
+    /// Representiert eine Kategorie
+    /// </summary>
     public class Kategorie
     {
-
+        private int id;
+        private string name;
+        private string beschreibung;
+        private List<Kategorie> subKategorien;
+        /// <summary>
+        /// Initialisiert die Kategorie und kopiert die Daten des DTO-Objekts auf das neu erstelle Kategorie Objekt.
+        /// </summary>
+        /// <param name="kategorie">Das Kategorie DTO Objekt</param>
         public Kategorie(Schnittstellen.Dto.Kategorie kategorie)
         {
-            this.kategorie = kategorie;
+            id = kategorie.Id;
+            name = kategorie.Name;
+            beschreibung = kategorie.Beschreibung;
+            subKategorien = kategorie.SubKategorien.Select(sk => new Kategorie(sk)).ToList();
         }
     }
 }
