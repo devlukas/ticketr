b1ce7dc1e9781ec548cebcc01ba9fefe543ebbba
Livio Brunner <livio.brunner@hotmail.com>
2016-06-19 14:05:47 +0200

    round usericons

--- src/Ticketr/Ticketr.UI/Components/EditTicketView/PersonDropdownItemViewModel.cs
+++ src/Ticketr/Ticketr.UI/Components/EditTicketView/PersonDropdownItemViewModel.cs
@@ -22,6 +22,16 @@ namespace Ticketr.UI.Components.EditTicketView
             get { return String.Format("{0} {1}", Vorname, Name); }
         }
 
+        public bool HasProfilePicture
+        {
+            get { return ProfilePicture != null && ProfilePicture.Length != 0; }
+        }
+
+        public string PersonInitials
+        {
+            get { return String.Format("{0}{1}", Vorname.ToUpper()[0], Name.ToUpper()[0]); }
+        }
+
         public int MitarbeiterId { get; set; }
 
         public int KundeId { get; set; }
